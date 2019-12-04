c67936f666d060ca14736ed80d61239f70f0961b
thanh2107 <tyn01685732770@gmail.com>
2019-12-04 10:19:06 +0700

    db

--- QuanLyTiecCuoi1/src/quanlytieccuoi/QuanLyTiecCuoi.java
+++ QuanLyTiecCuoi1/src/quanlytieccuoi/QuanLyTiecCuoi.java
@@ -26,7 +26,7 @@ public class QuanLyTiecCuoi extends Application {
     public void start(Stage stage) throws Exception {
 
 
-        Parent root = FXMLLoader.load(getClass().getResource("Sanh.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("DichVu.fxml"));
 
        // stage.initStyle(StageStyle.TRANSPARENT);
 
